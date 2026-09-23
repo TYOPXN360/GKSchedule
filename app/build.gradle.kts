0fa74c3df34997c489a999733e2301ba4227e112
TYOPXN360 <TYOPXN360@Gmail.com>
2026-09-23 22:20:11 +0800

    chore: 升版2.3.0/17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ty.gkschedule"
         minSdk = 31
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.2.2"
+        versionCode = 17
+        versionName = "2.3.0"
     }
 
     signingConfigs {
